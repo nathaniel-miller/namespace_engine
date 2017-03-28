d1ca91b525262ca53bcc2cded3247a602b298ea0
Nathaniel Miller <dukeoflaser@gmail.com>
2017-03-28 16:13:34 -0700

    Add executable.

--- rails_namespace_engine/lib/rails_namespace_engine.rb
+++ rails_namespace_engine/lib/rails_namespace_engine.rb
@@ -1,5 +1,32 @@
-require "rails_namespace_engine/version"
+require 'fileutils'
+require_relative "rails_namespace_engine/version"
 
-module RailsNamespaceEngine
+
+class RailsNamespaceEngine::CLI
+
+  def call(argv)
+    argv[0] = '--help' if argv.size < 2
+
+    if argv[0] == '--help'
+      puts instructions
+    else
+      @namespace = argv[0]
+      @engine_name = argv[1]
+    end
+  end
+
+
+
+  def instructions
+    insts = <<-INSTS
+      These are the instructions.
+    INSTS
+
+    insts
+  end
+
+
+
+  # FileUtils.mkdir 'engines'
   # Your code goes here...
 end
