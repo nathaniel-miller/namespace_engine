1c6a857868a208e95c6e71f6ad35fe45a4fd182d
Nathaniel Miller <dukeoflaser@gmail.com>
2017-03-28 16:29:17 -0700

    Tidy executable. Include proper initializer and getters/setters.

--- rails_namespace_engine/lib/rails_namespace_engine.rb
+++ rails_namespace_engine/lib/rails_namespace_engine.rb
@@ -3,19 +3,27 @@ require_relative "rails_namespace_engine/version"
 
 
 class RailsNamespaceEngine::CLI
+  attr_reader :help, :namespace, :engine_name
 
-  def call(argv)
+  def initialize(argv)
     argv[0] = '--help' if argv.size < 2
 
     if argv[0] == '--help'
-      puts instructions
+      @help = true
     else
+      @help = false
       @namespace = argv[0]
       @engine_name = argv[1]
     end
   end
 
-
+  def call
+    if help
+      puts instructions
+    else
+      generate
+    end
+  end
 
   def instructions
     insts = <<-INSTS
@@ -23,6 +31,10 @@ class RailsNamespaceEngine::CLI
     INSTS
 
     insts
+  end
+
+  def generate
+    puts "#{namespace} and #{engine_name}"
   end
 
 
