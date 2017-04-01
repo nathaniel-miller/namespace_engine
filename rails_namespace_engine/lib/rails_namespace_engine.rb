0975e856fa234a67e41e8de10bd7d9e720b8bbc3
Nathaniel Miller <dukeoflaser@gmail.com>
2017-04-01 08:17:54 -0700

    Refactor directory/file creation into seperate methods.

--- rails_namespace_engine/lib/rails_namespace_engine.rb
+++ rails_namespace_engine/lib/rails_namespace_engine.rb
@@ -4,8 +4,6 @@ require 'rails/generators'
 require_relative "rails_namespace_engine/version"
 
 class NamespaceEngine
-  # include Rails::Generators::Base
-
   attr_reader :help, :namespace, :engine_name
 
   def initialize(argv)
@@ -25,6 +23,9 @@ class NamespaceEngine
       puts instructions
     else
       create_directories
+      create_files
+      move_files
+      alter_files
     end
   end
 
@@ -41,18 +42,28 @@ class NamespaceEngine
     Rails::Generators.invoke('plugin', [engine_name])
     FileUtils.cd('../../../')
     FileUtils.mkdir './engines', verbose: true unless Dir.exists?('./engines')
-    FileUtils.mv "#{engine_name}", './engines', verbose: true
     FileUtils.mkdir "./engines/#{engine_name}/lib/#{namespace}", verbose: true
+  end
+
+  def create_files
+    FileUtils.touch "./engines/#{engine_name}/lib/#{namespace}_#{engine_name}.rb",
+      verbose: true
+    FileUtils.mv "./engines/#{engine_name}/#{engine_name}.gemspec",
+      "./engines/#{engine_name}/#{namespace}_#{engine_name}.gemspec",
+      verbose: true
+  end
+
+  def move_files
+    FileUtils.mv "#{engine_name}", './engines', verbose: true
     FileUtils.mv "./engines/#{engine_name}/lib/#{engine_name}",
       "./engines/#{engine_name}/lib/#{namespace}",
       verbose: true
     FileUtils.mv "./engines/#{engine_name}/lib/#{engine_name}.rb",
       "./engines/#{engine_name}/lib/#{namespace}",
       verbose: true
-    FileUtils.touch "./engines/#{engine_name}/lib/#{namespace}_#{engine_name}.rb",
-      verbose: true
-    FileUtils.mv "./engines/#{engine_name}/#{engine_name}.gemspec",
-      "./engines/#{engine_name}/#{namespace}_#{engine_name}.gemspec",
-      verbose: true 
+  end
+
+  def alter_files
+    # Add needed code to files
   end
 end
